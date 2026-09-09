51ec414cc8cb3013a3ac8ba128577e73b933cfd3
dracamilalino-prog <dracamilalinopsiq@gmail.com>
2026-09-09 08:17:26 -0300

    Adicionar app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.cah.personalstyle"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.cah.personalstyle"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
